1060b81d4dcd0c1e1d1eac6c629836bdf25ee6bf
Devin Jenkins <dvnjenks@gmail.com>
2019-05-11 19:33:23 -0700

    added some code to javascript,somewhat cofused

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,21 @@
-let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  
+//ADD ARRAY OF LETTERS
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var randomLetter =""
+var winCount = 0
+var lossCount = 0
+var guessesLeft = 0 
+var guessedLetters = []
+var guessesLeft
+
+//new game if guessed wrong
+function newGame() {
+    guessedLetters = [];
+    resetGuesses();
+    generateRandomLetter();
+
+//key press
+window.addEventListener("keypress", onKeyPress, false);
+newGame();    
+
+//im somewhat stuck at this point.....
